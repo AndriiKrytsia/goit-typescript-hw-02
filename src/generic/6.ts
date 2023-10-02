bf064d9c1c3b5f38657089c803a983f00e6364f2
Andrii Krytsia <124679055+AndriiKrytsia@users.noreply.github.com>
2023-10-02 19:21:13 +0200

    1

--- src/generic/6.ts
+++ src/generic/6.ts
@@ -14,7 +14,7 @@ type User = {
   password: string;
 };
 
-function createOrUpdateUser(initialValues: User) {
+function createOrUpdateUser({ email, name, surname, password }: User) {
   // Оновлення користувача
 }
 
